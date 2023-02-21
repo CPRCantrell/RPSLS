4ab3e166c2ae55d51e131d0eccd063a50df7f12f
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-02-21 17:15:29 -0500

    📦 NEW: Have completed the overriding of select gestures in the human child class

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ class Player:
     def __init__(self) -> None:
         self.points = 0
         self.gestures = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+        self.selected_gesture = None
 
     def select_gesture():
         pass
