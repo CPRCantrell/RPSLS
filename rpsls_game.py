e4c572b57c0e7cdb4dc7e373a9817898d1d7facd
Chris Pierson <cpierswim@gmail.com>
2023-02-21 16:52:39 -0500

    put in basic functions

--- rpsls_game.py
+++ rpsls_game.py
@@ -1,3 +1,25 @@
 class RpslsGame:
     def __init__(self) -> None:
+        self._player_list = []
+
+
+    def display_welcome(self) -> None:
+        pass
+
+    def perform_round(self) -> None:
+        pass
+
+    def determine_winner(player_one, gesture_one, player_two, gesture_two):
+        pass
+
+    def display_overall_winner() -> None:
+        pass
+
+    def display_rules() -> None:
+        pass
+
+    def play() -> None:
+        pass
+
+    def assign_players() -> None:
         pass
