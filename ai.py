eece33c46a506a7a770e16798670c6a69439c5d9
Chris Figueroa <125073370+CPRCantrell@users.noreply.github.com>
2023-02-21 18:09:47 -0500

    Revert "Player logic"

--- ai.py
+++ ai.py
@@ -1,10 +1,5 @@
 from player import Player
-import random as r
+
 class Ai(Player):
     def __init__(self) -> None:
-        self.name_list = ['M3gan', 'Bastion', 'BB8', 'R2D2', 'Data']
-        super().__init__(r.choice(self.name_list))
-
-    def select_gesture(self):
-        self.selected_gesture = r.choice(self.gestures)
-        return self.selected_gesture
+        super().__init__()
--- player.py
+++ player.py
@@ -1,12 +1,3 @@
 class Player:
-    def __init__(self, name) -> None:
-        self.name = name
-        self.points = 0
-        self.gestures = ['rock', 'paper', 'scissors', 'lizard', 'spock']
-        self.selected_gesture = None
-
-    def select_gesture():
+    def __init__(self) -> None:
         pass
-
-    def __str__(self) -> str:
-        self.selected_gesture
