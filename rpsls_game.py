7bdb749aa03bf3b52d25f0815518a6b7db3e6196
Chris Pierson <cpierswim@gmail.com>
2023-02-21 17:02:06 -0500

    added welcome message

--- rpsls_game.py
+++ rpsls_game.py
@@ -1,25 +1,36 @@
+from os import system, name
+
 class RpslsGame:
     def __init__(self) -> None:
         self._player_list = []
 
-
     def display_welcome(self) -> None:
-        pass
+        RpslsGame._clear_screen()
+        print("Welcome to the Rock, Paper, Scissors, Lizzard, Spock Game")
 
     def perform_round(self) -> None:
         pass
 
-    def determine_winner(player_one, gesture_one, player_two, gesture_two):
+    def determine_winner(self, player_one, gesture_one, player_two, gesture_two):
         pass
 
-    def display_overall_winner() -> None:
+    def display_overall_winner(self) -> None:
         pass
 
-    def display_rules() -> None:
+    def display_rules(self) -> None:
         pass
 
-    def play() -> None:
+    def play(self) -> None:
         pass
 
-    def assign_players() -> None:
+    def assign_players(self) -> None:
         pass
+
+    def _clear_screen() -> None:
+        # for windows
+        if name == 'nt':
+            _ = system('cls')
+    
+        # for mac and linux(here, os.name is 'posix')
+        else:
+            _ = system('clear')
