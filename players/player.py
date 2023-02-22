be1c7ef390fe0291d0910a57c2b72458fb56aa61
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-02-22 16:10:09 -0500

    👌 IMPROVE: clear screen before asking for gesture

--- players/player.py
+++ players/player.py
@@ -14,6 +14,7 @@ class Player:
 
     #Sets 2 players against eachother
     def battle(p1, p2):
+        Player._clear_screen()
         print(f'{p1.name} is going\n')
         p1.gesture = p1.gesture.set_gesture(p1.select_gesture())
         Player._clear_screen()
